d71affdedec48ef79cf465260124d2683a1ea322
Polar-Tsai <79502899+Polar-Tsai@users.noreply.github.com>
2022-01-27 09:33:45 +0800

    update
    細節修改，把讀取影片的程式碼改為讀取鏡頭，並把寫影片的程式全部都刪除

--- demo_culane.py
+++ demo_culane.py
@@ -42,10 +42,11 @@ if __name__ == "__main__":
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
 
+    cap = cv2.VideoCapture(0)
 
-    cap = cv2.VideoCapture("放入video.mp4路徑") # 放入video.mp4路徑
-    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    vout = cv2.VideoWriter(str(111) + '.avi', fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
+    #cap = cv2.VideoCapture("放入video.mp4路徑") # 放入video.mp4路徑
+    #fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+    #vout = cv2.VideoWriter(str(111) + '.avi', fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
 
     print("w = {}, h = {}".format(cap.get(3), cap.get(4)))
 
@@ -89,57 +90,15 @@ if __name__ == "__main__":
                     if out_j[k, i] > 0:
                         ppp = (int(out_j[k, i] * col_sample_w * cap.get(3) / 800) - 1, int(cap.get(4) - k * 20) - 1)
                         cv2.circle(frame,ppp,5,(0,255,0),-1)
-        vout.write(frame)
-
-    vout.release()
-
-
-
-    # if cfg.dataset == 'CULane':
-    #     splits = ['test0_normal.txt', 'test1_crowd.txt', 'test2_hlight.txt', 'test3_shadow.txt', 'test4_noline.txt', 'test5_arrow.txt', 'test6_curve.txt', 'test7_cross.txt', 'test8_night.txt']
-    #     datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root, 'list/test_split/'+split),img_transform = img_transforms) for split in splits]
-    #     img_w, img_h = 1640, 590
-    #     row_anchor = culane_row_anchor
-    # elif cfg.dataset == 'Tusimple':
-    #     splits = ['test.txt']
-    #     datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root, split),img_transform = img_transforms) for split in splits]
-    #     img_w, img_h = 1280, 720
-    #     row_anchor = tusimple_row_anchor
-    # else:
-    #     raise NotImplementedError
-    # for split, dataset in zip(splits, datasets):
-    #     loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle = False, num_workers=1)
-    #     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    #     print(split[:-3]+'avi')
-    #     vout = cv2.VideoWriter(split[:-3]+'avi', fourcc , 30.0, (img_w, img_h))
-    #     for i, data in enumerate(tqdm.tqdm(loader)):
-    #         imgs, names = data
-    #         imgs = imgs.cuda()
-    #         with torch.no_grad():
-    #             out = net(imgs)
-
-    #         col_sample = np.linspace(0, 800 - 1, cfg.griding_num)
-    #         col_sample_w = col_sample[1] - col_sample[0]
-
-
-    #         out_j = out[0].data.cpu().numpy()
-    #         out_j = out_j[:, ::-1, :]
-    #         prob = scipy.special.softmax(out_j[:-1, :, :], axis=0)
-    #         idx = np.arange(cfg.griding_num) + 1
-    #         idx = idx.reshape(-1, 1, 1)
-    #         loc = np.sum(prob * idx, axis=0)
-    #         out_j = np.argmax(out_j, axis=0)
-    #         loc[out_j == cfg.griding_num] = 0
-    #         out_j = loc
-
-    #         # import pdb; pdb.set_trace()
-    #         vis = cv2.imread(os.path.join(cfg.data_root,names[0]))
-    #         for i in range(out_j.shape[1]):
-    #             if np.sum(out_j[:, i] != 0) > 2:
-    #                 for k in range(out_j.shape[0]):
-    #                     if out_j[k, i] > 0:
-    #                         ppp = (int(out_j[k, i] * col_sample_w * img_w / 800) - 1, int(img_h * (row_anchor[cls_num_per_lane-1-k]/288)) - 1 )
-    #                         cv2.circle(vis,ppp,5,(0,255,0),-1)
-    #         vout.write(vis)
         
-    #     vout.release()
+
+    # 顯示圖片
+    cv2.imshow('live', frame)
+    #cv2.imshow('live', gray)
+
+    # 按下 q 鍵離開迴圈
+    if cv2.waitKey(1) == ord('q'):
+        break
+    # 釋放該攝影機裝置
+    cap.release()
+    cv2.destroyAllWindows
